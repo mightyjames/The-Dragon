1a311d580ccafbaf23c2d4c753a2885627007c4a
mightyjames <jamesgozarate1@gmail.com>
2016-02-03 15:56:25 -0500

    initial commit 4

--- Scripts/scenes/intro.js
+++ Scripts/scenes/intro.js
@@ -16,7 +16,7 @@ var scenes;
         // Start Method
         Intro.prototype.start = function () {
             // add Intro Image
-            this._introImage = new createjs.Bitmap("../../Assets/images/LeftCave.png");
+            this._introImage = new createjs.Bitmap("../../Assets/images/IntroCave.png");
             this.addChild(this._introImage);
             // add the LEFT_CAVE button to the MENU scene
             this._leftCaveButton = new objects.Button("LeftCaveButton", config.Screen.CENTER_X - 100, config.Screen.CENTER_Y + 180);
